2822c047a31d7dc9c585be1c7f5c443814e52b9b
Aikar6556 <266@cifpceuta.es>
2024-01-28 19:46:48 +0100

    Casa 28/01/2024 v2

---
+++ app/src/main/java/com/cifpceuta/pruebasqllite/Activity_Logo.java
@@ -0,0 +1,26 @@
+package com.cifpceuta.pruebasqllite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Activity_Logo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(Activity_Logo.this,Launcher.class));
+                finish();
+
+            }
+        },1500);
+
+    }
+}
